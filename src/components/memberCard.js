82bf2c9c964488f15c9a68f437b5b7d4e58efaab
TUANTMA-PC\Phuong.tt <phuong.tt@aris-vn.com>
2022-03-08 10:36:04 +0700

    update

---
+++ src/components/memberCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MemberCard = () => {
+  return (
+    <div className="wrapper">
+      <div className="card">
+        <div className="imgPart"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MemberCard;
