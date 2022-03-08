600c84398ae2ddc5c3f700d3c87c848d09037fd0
TUANTMA-PC\Phuong.tt <phuong.tt@aris-vn.com>
2022-03-08 10:52:41 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../global.css";
+import Script from "next/script";
+import Head from "next/head";
+import MemberCard from "../src/components/memberCard";
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Script
+        id="bootstrap-cdn"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      />
+      <Component {...pageProps} />
+      <main>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          <MemberCard />
+          <MemberCard />
+          <MemberCard />
+          <MemberCard />
+        </div>
+      </main>
+    </>
+  );
 }
-
-export default MyApp
