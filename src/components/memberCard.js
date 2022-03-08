5234acb88b898fae0396ffc67ae14933f995965e
TUANTMA-PC\Phuong.tt <phuong.tt@aris-vn.com>
2022-03-08 10:52:19 +0700

    update

--- src/components/memberCard.js
+++ src/components/memberCard.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const MemberCard = () => {
   return (
-    <div className="wrapper">
-      <div className="card">
-        <div className="imgPart"></div>
+    <div className="col">
+      <div className="card h-100">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
+        <div className="card-footer">
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </div>
       </div>
     </div>
   );
